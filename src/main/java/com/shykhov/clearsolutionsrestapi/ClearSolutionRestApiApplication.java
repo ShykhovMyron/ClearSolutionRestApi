25995b0bfd1c07aa23f275a20e315601c17b571b
Myron <shikhovmyron@gmail.com>
2022-09-14 15:31:04 +0200

    Initial commit

---
+++ src/main/java/com/shykhov/clearsolutionsrestapi/ClearSolutionRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.shykhov.clearsolutionsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSolutionRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearSolutionRestApiApplication.class, args);
+    }
+
+}
