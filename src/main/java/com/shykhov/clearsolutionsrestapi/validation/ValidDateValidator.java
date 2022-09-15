6a266df777ab99dcb2e4da0612b4f600dcc1119c
Myron <shikhovmyron@gmail.com>
2022-09-15 23:41:07 +0200

    create age validation annotation

---
+++ src/main/java/com/shykhov/clearsolutionsrestapi/validation/ValidDateValidator.java
@@ -0,0 +1,32 @@
+package com.shykhov.clearsolutionsrestapi.validation;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ValidDateValidator implements ConstraintValidator<ValidAge, LocalDate> {
+
+    @Value("${user.age}")
+    public int age;
+
+    private static boolean isValidAge(LocalDate dateBirth, int age) {
+        if (dateBirth != null) {
+            return LocalDate.now().minus(age, ChronoUnit.YEARS).isAfter(dateBirth) ||
+                    LocalDate.now().minus(age, ChronoUnit.YEARS).isEqual(dateBirth);
+        } else return false;
+    }
+
+    @Override
+    public void initialize(ValidAge constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(LocalDate dateBirth, ConstraintValidatorContext constraintValidatorContext) {
+
+        return isValidAge(dateBirth, age);
+    }
+}
