911c2375d8d5a5d8dc606958e15d51792438e381
Myron <shikhovmyron@gmail.com>
2022-09-15 23:42:57 +0200

    add query for selecting users for the specified period of time

--- src/main/java/com/shykhov/clearsolutionsrestapi/dao/repository/UserRepository.java
+++ src/main/java/com/shykhov/clearsolutionsrestapi/dao/repository/UserRepository.java
@@ -2,6 +2,14 @@ package com.shykhov.clearsolutionsrestapi.dao.repository;
 
 import com.shykhov.clearsolutionsrestapi.dao.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    @Query(value = "SELECT u FROM UserEntity u WHERE u.birthDate BETWEEN :from AND :to")
+    List<UserEntity> getUsersTimeInterval(@Param("from") LocalDate fromDate, @Param("to") LocalDate toDate);
 }
