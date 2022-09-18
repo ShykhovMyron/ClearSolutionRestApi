8dac8c0390ba082d477840487d986334199fb636
Myron <shikhovmyron@gmail.com>
2022-09-18 11:06:11 +0200

    add validation exception handler

--- src/main/java/com/shykhov/clearsolutionsrestapi/exeption/GeneralExceptionHandler.java
+++ src/main/java/com/shykhov/clearsolutionsrestapi/exeption/GeneralExceptionHandler.java
@@ -1,17 +1,26 @@
 package com.shykhov.clearsolutionsrestapi.exeption;
 
 import com.shykhov.clearsolutionsrestapi.exeption.custom.CustomExceptionDefault;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-@Controller
-public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GeneralExceptionHandler {
+
+    private final BasicErrorController errorController;
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request) {
@@ -23,5 +32,12 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleAnyException(CustomExceptionDefault ex, WebRequest request) {
         ex.printStackTrace();
         return new ResponseEntity<>(ex.getLocalizedMessage(), new HttpHeaders(), BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class, BindException.class})
+    public ResponseEntity<Map<String, Object>> defaultErrorHandler(Exception ex, HttpServletRequest request) {
+        request.setAttribute("javax.servlet.error.request_uri", request.getPathInfo());
+        request.setAttribute("javax.servlet.error.status_code", 400);
+        return errorController.error(request);
     }
 }
